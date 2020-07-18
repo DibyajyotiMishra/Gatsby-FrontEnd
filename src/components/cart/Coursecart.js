3ae35a75e4876f4ec5f94a1283c5706d2be3e6a8
DibyajyotiMishra <dibyajyotimishra14@gmail.com>
2020-07-19 00:57:07 +0530

    filter

--- src/components/cart/Coursecart.js
+++ src/components/cart/Coursecart.js
@@ -1,19 +1,64 @@
 import React, { Component } from "react"
 import Img from "gatsby-image"
 
+const getCategory = items => {
+  let holdItems = items.map(items => {
+    return items.node.category
+  })
+  let holdCategories = new Set(holdItems)
+  let categories = Array.from(holdCategories)
+  categories = ["all", ...categories]
+  return categories
+}
+
 export default class Coursecart extends Component {
   constructor(props) {
     super(props)
     this.state = {
       courses: props.courses.edges,
       mycourses: props.courses.edges,
+      mycategories: getCategory(props.courses.edges),
     }
   }
+
+  categoryClicked = category => {
+    let safe = [...this.state.courses]
+
+    if (category === "all") {
+      this.setState(() => {
+        return { mycourses: safe }
+      })
+    } else {
+      let selected = safe.filter(({ node }) => node.category === category)
+      this.setState(() => {
+        return { mycourses: selected }
+      })
+    }
+  }
+
   render() {
     return (
       <section className="py-5">
         <div className="container">
           <h1 className="text-center display-3">Courses</h1>
+          <div className="row my-3">
+            <div className="col-10 mx-auto text-center">
+              {this.state.mycategories.map((category, index) => {
+                return (
+                  <button
+                    type="button"
+                    className="m-3 px-3 btn btn-light"
+                    key={index}
+                    onClick={() => {
+                      this.categoryClicked(category)
+                    }}
+                  >
+                    {category}
+                  </button>
+                )
+              })}
+            </div>
+          </div>
           <div className="row">
             {this.state.mycourses.map(({ node }) => {
               return (
